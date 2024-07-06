e4f0cda118b82109ed51c243f7d04ef6fedc6fc9
chemguide <argentum198@gmail.com>
2024-07-06 20:32:53 +0500

    check hw

---
+++ src/main/java/com/example/EmployeeBookMap/hw28/EmployeeAlreadyAddedException.java
@@ -0,0 +1,11 @@
+package com.example.EmployeeBookMap.hw28;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.ALREADY_REPORTED)
+public class EmployeeAlreadyAddedException extends RuntimeException{
+    public EmployeeAlreadyAddedException() {
+        super();
+    }
+}
